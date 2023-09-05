d74a2f3467f4c93e2ff639d2f8cfb43f89e2a6a9
panjing <2855538894@qq.com>
2023-09-05 17:27:02 +0800

    根据流程实例查询,设置变量等

--- activiti7Demo/src/main/java/org/example/controller/MyTestController.java
+++ activiti7Demo/src/main/java/org/example/controller/MyTestController.java
@@ -1,11 +1,17 @@
 package org.example.controller;
 
+
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.DeploymentQuery;
 import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.runtime.ProcessInstanceQuery;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.task.TaskQuery;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +20,9 @@ import javax.annotation.Resource;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName MyTestController
@@ -28,9 +36,12 @@ public class MyTestController {
     RepositoryService repositoryService;
     @Resource
     RuntimeService runtimeService;
+    @Resource
+    TaskService taskService;
 
     /**
      * 根据bpmn图部署流程
+     *
      * @param filename filename
      * @return String String
      */
@@ -54,15 +65,16 @@ public class MyTestController {
      * 根据部署信息查询部署id
      * 再根据部署id查询流程定义的id
      * 部署名称是可以重复的
+     *
      * @param name name
      * @return Object Object
      */
     @GetMapping("/deploy/query/{deployName}")
-    public Object queryDeploy(@PathVariable("deployName") String name){
+    public Object queryDeploy(@PathVariable("deployName") String name) {
         //根据名称获取流程部署id
         DeploymentQuery deployQuery = repositoryService.createDeploymentQuery();
         List<Deployment> deployments = deployQuery.deploymentName(name).list();
-        if(deployments.size() == 0){
+        if (deployments.size() == 0) {
             return new Object();
         }
         //根据部署id获取流程定义id
@@ -72,7 +84,7 @@ public class MyTestController {
             List<ProcessDefinition> definitions = repositoryService.createProcessDefinitionQuery()
                     .deploymentId(deployment.getId())
                     .list();
-            if (definitions.size()>0) {
+            if (definitions.size() > 0) {
                 //一个部署id可以对应多个流程定义id
                 for (ProcessDefinition definition : definitions) {
                     processDefinitionIds.add(definition.getId());
@@ -86,19 +98,73 @@ public class MyTestController {
      * 根据流程定义id启动一个流程实例
      * 流程id (long) 和流程key(String 长度255)是唯一的
      * 都可以作为启动实例的入参
+     *
      * @return String String
      */
     @GetMapping("/start/{processDefineId}")
-    public String startProcess(@PathVariable("processDefineId")String proDefId){
+    public String startProcess(@PathVariable("processDefineId") String proDefId) {
         //根据流程定义key启动流程
 //        runtimeService.startProcessInstanceByKey("myProcessDefinitionKey");
         //根据流程定义id启动流程
         ProcessInstance proInstance = runtimeService.startProcessInstanceById(proDefId);
         //业务id->BusinessKey自己设置,但没有强制要求唯一,索引形式是normal
-        return "流程实例主键:"+proInstance.getId()
-                +"\n流程实例关联的业务id:" + proInstance.getBusinessKey();
+        return "流程实例主键:" + proInstance.getId()
+                + "\n流程实例关联的业务id:" + proInstance.getBusinessKey();
     }
 
+
+    /**
+     * 根据流程实例ID查询当前活动的任务。
+     */
+    @GetMapping("/task/{processInstanceId}")
+    public String queryActiveTask(@PathVariable("processInstanceId") String processInstanceId) {
+        TaskQuery taskQuery = taskService.createTaskQuery().processInstanceId(processInstanceId);
+        List<Task> tasks = taskQuery.list();
+        System.out.println(tasks);
+        return tasks.toString();
+    }
+
+    /**
+     * 根据任务id完成任务
+     */
+    @GetMapping("/task/complete/{taskId}")
+    public String completeTaskById(@PathVariable("taskId") String taskId) {
+        taskService.complete(taskId);
+        return "任务完成";
+    }
+
+    /**
+     * 根据流程实例id,查询流程实例对象信息
+     */
+    @GetMapping("/processInstance/{id}")
+    public ProcessInstance getAllInfoOfProcessInstance(@PathVariable("id") String id) {
+        ProcessInstanceQuery query = runtimeService.createProcessInstanceQuery().processInstanceId(id);
+        ProcessInstance processInstance = query.singleResult();
+        System.out.println(processInstance);
+        return processInstance;
+    }
+
+    /**
+     * 为流程实例自定义变量
+     */
+    @GetMapping("/processInstance/set/{id}/{key}/{value}")
+    public String setVariable4ProcessInstance(@PathVariable("id") String id,
+                                              @PathVariable("key") String key,
+                                              @PathVariable("value") String value) {
+        Map<String, String> map = new HashMap<>();
+        map.put(key, value);
+        runtimeService.setVariables(id, map);
+        return "已为流程" + id + "设置变量" + key + ":" + value;
+    }
+
+    /**
+     * 查询为流程实例自定义的变量
+     */
+    @GetMapping("/processInstance/get/{id}/{key}")
+    public String getVariable4ProcessInstance(@PathVariable("id") String id, @PathVariable("key") String key) {
+        Object variable = runtimeService.getVariable(id, key);
+        return "流程" + id + "自定义的变量" + key + "的值为:" + variable;
+    }
 
 
 }
--- activiti7Demo/src/test/java/org/example/MySpringInitializerWebApplicationTests.java
+++ activiti7Demo/src/test/java/org/example/MySpringInitializerWebApplicationTests.java
@@ -1,12 +1,11 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MySpringInitializerWebApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
